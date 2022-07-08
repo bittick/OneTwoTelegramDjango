9e4467bd774fb28b4240b724806cec92c40f4449
simba-l-l <prokhorovm.v@mail.ru>
2022-07-09 01:20:55 +0300

    добавление ордера на тест

--- OneTwoTelegram/store/api.py
+++ OneTwoTelegram/store/api.py
@@ -1,10 +1,10 @@
 from .models import *
 from rest_framework import viewsets, permissions
 from .serializers import *
-from rest_framework.decorators import api_view, action, parser_classes
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import json
-from rest_framework.parsers import JSONParser
+
 
 class SneakersViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
@@ -50,33 +50,24 @@ class OrderListViewSet(viewsets.ModelViewSet):
     queryset = OrderList.objects.all()
 
 
-@api_view(['POST', 'GET'])
-@parser_classes([JSONParser])
+@api_view(['POST'])
 def add_order(request):
-    order_info = request
-    print(type(request), request.__dict__)
-    # order = OrderList(
-    #     customer=order_info['order_list']['customer'],
-    #     shipping_address=order_info['order_list']['shipping_address'],
-    #     phone_number=order_info['order_list']['phone_number']
-    # )
-    # print(order)
-    # # order.save()
-    # for item in order_info['order_items']:
-    #     cur_item = OrderItem(
-    #         order_id=order.id,
-    #         sneaker_id=item['sneaker_id'],
-    #         sneaker_size=item['sneaker_size'],
-    #         quantity=item['quantity']
-    #     )
-    #     print(cur_item)
-    #     # cur_item.save()
+    order_info = json.loads(request.body.decode("utf-8"))
+    order = OrderList(
+        customer=order_info['order_list']['customer'],
+        shipping_address=order_info['order_list']['shipping_address'],
+        phone_number=order_info['order_list']['phone_number']
+    )
+    print(order)
+    order.save()
+    for item in order_info['order_items']:
+        sneaker = Sneaker.objects.get(id=item['sneaker_id'])
+        cur_item = OrderItem(
+            order_id=order,
+            sneaker_id=sneaker,
+            sneaker_size=item['sneaker_size'],
+            quantity=item['quantity']
+        )
+        print(cur_item)
+        cur_item.save()
     return Response(200)
-
-
-# @api_view(['GET'])
-# def get_order(request):
-#     list = OrderList.objects.all()
-#     # items = list.order_idOf.all()
-#     a = json.dumps(list)
-#     return Response(a)
