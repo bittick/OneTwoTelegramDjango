a31eb0bf1b37157eb5daac44ef3ae5c99b5345cc
pashtonchik <pashka191@yandex.ru>
2022-07-12 14:21:35 +0300

    sizes = bool

--- store/admin.py
+++ store/admin.py
@@ -9,19 +9,27 @@ class OrderItemInline(admin.TabularInline):
     extra = 0
     classes = ('collapse',)
 
+
 class SizesSneakerInline(admin.StackedInline):
     model = SizesSneaker
     min_num = 1
     max_num = 1
-    classes = ('collapse', )
+    classes = ('collapse',)
+
+    fields = (('size_350', 'size_355', 'size_360', 'size_365'),
+              ('size_370', 'size_375', 'size_380', 'size_385'),
+              ('size_390', 'size_400', 'size_405', 'size_410'),
+              ('size_420', 'size_425', 'size_430', 'size_440'),
+              ('size_445', 'size_450', 'size_460', 'size_465'),)
+
 
 @admin.register(Sneaker)
 class SneakersAdmin(admin.ModelAdmin):
     list_display = ('title', 'brand', 'price')
 
     def get_images(self, obj):
-        html_images = f'''<img src={obj.image1.url} height="100">'''\
-                      + f'''<img src={obj.image2.url} height="100">'''\
+        html_images = f'''<img src={obj.image1.url} height="100">''' \
+                      + f'''<img src={obj.image2.url} height="100">''' \
                       + f'''<img src={obj.image3.url} height="100">'''
         if obj.image4:
             html_images += f'''<img src={obj.image4.url} height="100">'''
@@ -43,24 +51,10 @@ class SneakersAdmin(admin.ModelAdmin):
 
 @admin.register(OrderList)
 class OrderListAdmin(admin.ModelAdmin):
-    
-    def get_items(self, obj):
-        text = ''
-
-        if isinstance(obj.items, list):
-            for item in obj.items:
-                text += f'Товар: {item["sneaker_title"]}, Размер: {item["size"]}, Количество: {item["quantity"]}\n'
-        else:
-            text = f'Товар: {obj.items["sneaker_title"]}, Размер: {obj.items["size"]}, Количество: {obj.items["quantity"]}'
-        return text
-
-    readonly_fields = ('get_items', )
 
     list_display = ('id', 'registration_date', 'customer', 'is_paid', 'given_to_work', 'delivered', 'phone_number')
+    search_fields = ('id', 'customer')
 
     inlines = [
         OrderItemInline,
     ]
-
-
-
